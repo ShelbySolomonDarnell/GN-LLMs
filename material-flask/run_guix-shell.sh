7adb1abe8071654cd75a1ccf2722410916984906
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-08-03 17:31:08 +0300

    Replace --expose with --share to make things writable
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- material-flask/run_guix-shell.sh
+++ material-flask/run_guix-shell.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-guix shell bash coreutils zsh which vim --expose=$PWD -C --network -f guix.scm -- env SCRIPT_NAME=/ai gunicorn --config $PWD/gunicorn-cfg.py run:app
+guix shell bash coreutils zsh which vim --share=$PWD -C --network -f guix.scm -- env SCRIPT_NAME=/ai gunicorn --config $PWD/gunicorn-cfg.py run:app
