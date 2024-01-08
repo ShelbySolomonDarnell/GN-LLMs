429a4774db2d22cca0dadecc9b553dbf6a7d4697
ShelbySolomonDarnell <shelbysdarnell@gmail.com>
2024-01-08 17:50:36 +0300

    Small menu changes and id manipuation

--- material-flask/apps/apihandler/response.py
+++ material-flask/apps/apihandler/response.py
@@ -14,11 +14,15 @@ basedir           = os.path.abspath(os.path.dirname(__file__))
 class DocIDs():
     def __init__(self):
         # open doc ids for GN refs
-        self.doc_ids = self.loadFile("doc_ids.json")
+        self.doc_ids = {} #self.loadFile("doc_ids.json")
         # open doc ids for Diabetes references
-        self.sugar_doc_ids = self.loadFile("sugar_doc_ids.json")
+        #self.sugar_doc_ids = self.loadFile("sugar_doc_ids.json")
         # format is not what I prefer, it needs to be rebuilt
-        self.formatDocIDs(self.sugar_doc_ids)
+        #self.formatDocIDs(self.sugar_doc_ids)
+        # open all doc ids file
+        self.all_doc_ids = self.loadFile("all_files.json")
+        self.formatDocIDs(self.all_doc_ids)
+
 
     def loadFile(self, file_name):
         file_path = os.path.join(basedir, file_name)
@@ -29,7 +33,7 @@ class DocIDs():
             return result
         else:
             raise Exception("\n{0} -- File does not exist\n".format(file_path))
-    
+
     def formatDocIDs(self, values):
         for _key, _val in values.items():
             #print("Key {0}".format(_key))
@@ -42,8 +46,8 @@ class DocIDs():
                     self.doc_ids.update({docID: docName})
 
     def formatDocumentName(self, val):
-       result = val.removesuffix('.pdf') 
-       result = result.removesuffix('.txt') 
+       result = val.removesuffix('.pdf')
+       result = result.removesuffix('.txt')
        result = result.replace('_', ' ')
        return result
 
@@ -107,3 +111,24 @@ def createAccordionFromJson(theContext):
 
 
 the_doc_ids = DocIDs()
+
+"""
+%s/{*,\n"filename"/{"filename"/
+:%s/author=/"author": /
+:%s/journal=/"journal": /
+:%s/url=/"url": /
+:%s/doi=/"doi": /
+:%s/issn=/"issn": /
+:%s/urldate=/"urldate": /
+:%s/volume=/"volume": /
+:%s/number=/"number": /
+:%s/pages=/"pages": /
+:%s/year=/"year": /
+:%s/month=/"month": /
+:%s/publisher=/"publisher": /
+%s/:\s{/: "/
+%s/*},/\*",/
+%s/[a-z]\+},/[
+
+Welcome to the GeneNetwork Question and Answer (GNQA)system. We utilize a large language model and 3000 scientific publications to make GNQA a subject matter expert in three areas: GeneNetwork.org research, genomics/genetics with regards to diabetes, and genomics/genetics with regards to aging.  At the moment when you ask GNQA something it will attempt to return a sensible answer with ``real'' references. To this end we aim to reduce hallucinations and provide a knowledge launchpad for a researcher to enhance their knowledge on the relevant subject matter.  GNQA is not a finished product as we are working diligently to improve it daily. Thanks for using GNQA!
+"""
